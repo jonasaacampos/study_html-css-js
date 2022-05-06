5280c9ddeb2de3364b2cf3be3c3bdc4a5ca2fef6
jaac <jonasaacampos@gmail.com>
2022-05-05 21:54:19 -0300

    Anotações sobre DOM

--- 4_dom/Sistema de cadastro de imóveis/js/app.js
+++ 4_dom/Sistema de cadastro de imóveis/js/app.js
@@ -1,9 +1,9 @@
 class App {
     adicionarImovel() {
         event.preventDefault()
-        let imovelTipo = document.querySelector("input[name='tipo-imovel']").checked
+        let imovelTipo = document.querySelector("input[name='tipo-imovel']").value
         let imovelArea = document.querySelector("input[name='area']").value
-        let imovelAlugado = document.querySelector("select[name='imovel-alugado-check']").value
+        let imovelAlugado = document.querySelector("select[name='imovel-alugado-check']").checked
 
         let imovelCadastrado = new imovel(imovelTipo, imovelArea, imovelAlugado)
         this.adicionarImovelALista(imovelCadastrado)
@@ -12,18 +12,38 @@ class App {
 
     adicionarImovelALista(imovelCadastrado) {
         let listaElementos = document.createElement("li")
-        let imoveilInfo = `Tipo: ${imovelCadastrado.tipo},
+        let imovelInfo = `Tipo: ${imovelCadastrado.tipo},
                            Área: ${imovelCadastrado.area}m²`
         if (imovelCadastrado.alugado) {
             let tagImovelAlugado = this.criarTagAlugado()
             listaElementos.appendChild(tagImovelAlugado)
         }
-        listaElementos.innerHTML += imoveilInfo
-
-
-
+        listaElementos.innerHTML += imovelInfo
+        let criarTagAlugado = this.criarTagAlugado()
+        listaElementos.appendChild(criarTagAlugado)
+        document.appendChild(criarTagAlugado)
     }
 
+    criarTagAlugado() {
+        let tagAlugado = document.createElement("span")
+        tagAlugado.style.color = "white"
+        tagAlugado.style.backgroundColor = "red"
+        tagAlugado.innerText = "Alugado!!!"
+        return tagAlugado
+    }
+
+    criarBtnRemover(){
+        let botaoRemoverItemLista = document.createElement("button")
+        botaoRemoverItemLista.setAttribute("onclik", "app.remove()")
+        botaoRemoverItemLista.innerText =  "Remover"
+        return botaoRemoverItemLista
+    }
+
+    limparFormulario() {
+        let removerItemLista = event.target.parentNode
+        document.getElementById("imoveis-registrados")
+
+    }
 }
 
 
